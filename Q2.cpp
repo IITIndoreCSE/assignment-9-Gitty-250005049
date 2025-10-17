3406f2f6f85821516697c62b556e1bf9a77bfba2
Gitty-250005049 <mems250005049@iiti.ac.in>
2025-10-17 15:51:13 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -1,1 +1,29 @@
-
+#include <iostream>
+using namespace std;
+struct Color {
+    int red;
+    int green;
+    int blue;
+};
+int main() {
+    Color colors[5] = {
+        {120, 150, 200}, // First color
+        {50, 100, 150},  // Second color
+        {200, 250, 100}, // Third color
+        {75, 125, 175},  // Fourth color
+        {10, 20, 30}     // Fifth color
+    };
+    for (int i = 0; i < 3; i++) {
+        colors[i].red = 255 - colors[i].red;
+        colors[i].green = 255 - colors[i].green;
+        colors[i].blue = 255 - colors[i].blue;
+    }
+    cout << "Updated RGB values for all 5 colors:\n";
+    for (int i = 0; i < 5; i++) {
+        cout << "Color " << i + 1 << ": ";
+        cout << "Red = " << colors[i].red << ", ";
+        cout << "Green = " << colors[i].green << ", ";
+        cout << "Blue = " << colors[i].blue << endl;
+    }
+    return 0;
+}
